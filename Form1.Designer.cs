576ddd9e5c31f43c91ea3428cf165d48eb685f85
jonathan82006 <jonathanqlejandro@gmail.com>
2025-10-11 15:17:38 -0600

    Prueba

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -76,7 +76,7 @@
             button1.Name = "button1";
             button1.Size = new Size(183, 39);
             button1.TabIndex = 4;
-            button1.Text = "Login";
+            button1.Text = "Logi";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
