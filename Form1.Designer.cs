a5c753b6f7b6aa2985086ed9154574158343995b
jonathan82006 <jonathanqlejandro@gmail.com>
2025-10-11 12:02:18 -0600

    Initial commit: Windows Forms project

---
+++ Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PetitPlannerIntegrador
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBoxUsuario = new TextBox();
+            textBoxPassword = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(307, 55);
+            label1.Name = "label1";
+            label1.Size = new Size(72, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Usuario";
+            label1.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(307, 136);
+            label2.Name = "label2";
+            label2.Size = new Size(101, 25);
+            label2.TabIndex = 1;
+            label2.Text = "Contraseña";
+            label2.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // textBoxUsuario
+            // 
+            textBoxUsuario.Location = new Point(307, 83);
+            textBoxUsuario.Name = "textBoxUsuario";
+            textBoxUsuario.Size = new Size(183, 31);
+            textBoxUsuario.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            textBoxPassword.Location = new Point(307, 164);
+            textBoxPassword.Name = "textBoxPassword";
+            textBoxPassword.PasswordChar = '*';
+            textBoxPassword.Size = new Size(183, 31);
+            textBoxPassword.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(307, 229);
+            button1.Name = "button1";
+            button1.Size = new Size(183, 39);
+            button1.TabIndex = 4;
+            button1.Text = "Login";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(962, 548);
+            Controls.Add(button1);
+            Controls.Add(textBoxPassword);
+            Controls.Add(textBoxUsuario);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBoxUsuario;
+        private TextBox textBoxPassword;
+        private Button button1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using MySqlConnector;
+namespace PetitPlannerIntegrador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Asumiendo que tienes TextBox para usuario y contraseña
+            string usuario = textBoxUsuario.Text;
+            string password = textBoxPassword.Text;
+
+            // Validar que no estén vacíos
+            if (string.IsNullOrEmpty(usuario) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Por favor, complete todos");
+                return;
+            }
+
+            string connectionString = "Server=localhost;Database=petitplanner;Uid=root;Pwd=root;";
+
+            try
+            {
+                using (MySqlConnection connection = new MySqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // Consulta para verificar credenciales
+                    string query = "SELECT COUNT(*) FROM usuarios WHERE user = @usuario AND password = @password";
+
+                    MySqlCommand command = new MySqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@usuario", usuario);
+                    command.Parameters.AddWithValue("@password", password); // En producción, usa hash!
+
+                    int count = Convert.ToInt32(command.ExecuteScalar());
+
+                    if (count > 0)
+                    {
+                        // Aquí abres el siguiente formulario
+                        //home formPrincipal = new home();
+                        //formPrincipal.Show();      // Muestra el nuevo formulario
+                        //this.Hide();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario o contraseña incorrectos");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+            }
+        }
+    }
+}
